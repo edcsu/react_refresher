8c0244ca78eadcbe90a65e04da632d06d485c4f5
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-03-21 12:52:54 +0300

    feat: lifting state

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react"
 import NewPost from "./NewPost"
 import Post from "./Post"
 import classes from "./PostsList.module.css"
 
 const PostList = () => {
+    const [enteredBody, setEnteredBody] = useState('')
+    const [enteredAuthor, setEnteredAuthor] = useState('')
+    
+    const changeBodyHandler = (event) => {
+        setEnteredBody(event.target.value)
+    }
+    const changeAuthorHandler = (event) => {
+        setEnteredAuthor(event.target.value)
+    }
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
             <ul className={classes.post}>
-                <Post name='Keith' body='React.js is awesome' />
+                <Post name={enteredAuthor} body={enteredBody} />
                 <Post name='Edwin' body='Next.js is awesome' />
             </ul>
         </>
