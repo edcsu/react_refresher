8b4481b71acb3a35e7c1b1f85627164672674b6c
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-03-21 12:25:31 +0300

    style: add new post component

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost"
 import Post from "./Post"
 import classes from "./PostsList.module.css"
 
 const PostList = () => {
     return (
-        <ul className={classes.post}>
-            <Post name='Keith' body='React.js is awesome' />
-            <Post name='Edwin' body='Next.js is awesome' />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.post}>
+                <Post name='Keith' body='React.js is awesome' />
+                <Post name='Edwin' body='Next.js is awesome' />
+            </ul>
+        </>
     )
 }
 
