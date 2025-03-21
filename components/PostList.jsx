c5dc699fe126cfa16218600a870e4ec39fbe1454
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-03-21 16:28:28 +0300

    refactor: add post to list

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -2,13 +2,20 @@ import NewPost from "./NewPost"
 import Post from "./Post"
 import classes from "./PostsList.module.css"
 import Modal from "./Modal"
+import { useState } from "react"
 
 const PostList = ({isPosting, onStopPosting}) => {
-    
+    const [postArray, setPostArray] = useState([])
+
+    const addPostHandler = (postData) => {
+        setPostArray((previousPosts) => {
+            [ postData, ...previousPosts]
+        })
+    }
     return (
         <>
             {isPosting && (<Modal onClose={onStopPosting}>
-                <NewPost onCancel={onStopPosting} />
+                <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
             </Modal>)}
             <ul className={classes.post}>
                 <Post name='Edwin' body='Next.js is awesome' />
